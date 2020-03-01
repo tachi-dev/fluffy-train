8c061d1fa26ebf4644a7f8f10c43fc24a93f120c
tachi-dev <61638591+tachi-dev@users.noreply.github.com>
2020-03-01 09:44:10 +0900

    High and Lowを新規作成

---
+++ HighandLow/app/src/main/java/com/example/tachi/highandlow/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.tachi.highandlow
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
